5432cae05eb32f577bee498508b0dc1f33ed86ff
rajathirumal <rajathirumal98@gmail.com>
2023-10-30 19:18:24 +0530

    CLI --> BInit, BCompile

---
+++ bin/BInit.bat
@@ -0,0 +1,3 @@
+@echo off
+set "PARENT=bridge"
+python3.11 "%PARENT%\init.py" init
---
+++ bridge/init.py
@@ -0,0 +1,40 @@
+import click
+
+import utils
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name="init", help="Prepare the tables")
+def _init():
+    """This function makes sure the following
+    1. Properties file readability check.
+    2. Test connection to the DB.
+    3. Bridge funciton args preparation.
+    """
+    click.echo("Hello")
+
+
+@cli.command(name="BCompile", help="Compile the bridge code")
+@click.option(
+    "-s",
+    "--source",
+    type=str,
+    required=True,
+    help="Full path including the file name",
+)
+def _compile(source):
+    """This function,
+    0. `Check if init`        : Init creates args for the bridge function
+    1. `Syntax analysis`      : Bridge syntax analysis
+    2. `Semantic analysis`    : Bridge logic validation
+    3. `Code generations`     : Prepare a SQL with the function
+    """
+    click.echo(source)
+
+
+if __name__ == "__main__":
+    cli()
